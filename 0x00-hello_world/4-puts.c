b5b1a501c7eb0dde4853961da44205afced96784
Oladayo <sholayoc@gmail.com>
2023-09-11 19:06:33 -0700

    was omiting the capital letter on line 3 Entry

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
- * return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main(void)
+int main(void);
 {
-    puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
